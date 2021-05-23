b10ad167e03046cdd31ed6a155206e86c258c65d
ILya Turukin <iturukin@eastbanctech.com>
2021-05-23 23:08:12 +0700

    [#1] Made project multi module
    Added 'cache' and 'common' modules.

---
+++ api/src/test/java/covid19api/facade/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package covid19api.facade.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
