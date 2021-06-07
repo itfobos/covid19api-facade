f7d009b0cd75ba8897b63fcf5f67505fa9d2a28c
ILya Turukin <iturukin@eastbanctech.com>
2021-06-07 22:01:24 +0700

    [#3] Added date time request params.

--- api/src/main/java/covid19api/facade/api/ApiCallTestController.java
+++ api/src/main/java/covid19api/facade/api/ApiCallTestController.java
@@ -3,21 +3,27 @@ package covid19api.facade.api;
 import covid19api.facade.api.client.CovidApiClient;
 import covid19api.facade.api.client.DayInfoRecord;
 import org.hibernate.validator.constraints.Length;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.ConstraintViolationException;
+import java.time.ZonedDateTime;
 import java.util.Collection;
 
 @RestController
 @RequestMapping("/api/v1")
 @Validated
 public class ApiCallTestController {
+    private final static Logger logger = LoggerFactory.getLogger(ApiCallTestController.class);
     private final CovidApiClient covidApiClient;
 
     public ApiCallTestController(CovidApiClient covidApiClient) {
@@ -26,8 +32,22 @@ public class ApiCallTestController {
 
 
     @GetMapping("/country/{countrySlug}")
-    public Collection<DayInfoRecord> byCountry(@PathVariable @Length(min = 3) String countrySlug) {
-        return covidApiClient.byCountry(countrySlug);
+    public Collection<DayInfoRecord> byCountry(@PathVariable @Length(min = 3) String countrySlug,
+
+                                               @RequestParam(required = false)
+                                               @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                                                       ZonedDateTime from,
+
+                                               @RequestParam(required = false)
+                                               @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+                                                       ZonedDateTime to) {
+        if (from != null && to != null) {
+            return covidApiClient.byCountry(countrySlug, from, to);
+        } else {
+            logger.info("No dates interval defined. Requesting data for last year.");
+            final ZonedDateTime now = ZonedDateTime.now();
+            return covidApiClient.byCountry(countrySlug, now.minusYears(1), now);
+        }
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
--- api/src/main/java/covid19api/facade/api/client/CovidApiClient.java
+++ api/src/main/java/covid19api/facade/api/client/CovidApiClient.java
@@ -4,12 +4,16 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import java.time.ZonedDateTime;
 import java.util.Collection;
 
 @FeignClient(name = "covid19api", url = "https://api.covid19api.com/")
 public interface CovidApiClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "/country/{countrySlug}")
-    Collection<DayInfoRecord> byCountry(@PathVariable("countrySlug") String countrySlug);
+    Collection<DayInfoRecord> byCountry(@PathVariable("countrySlug") String countrySlug,
+                                        @RequestParam ZonedDateTime from,
+                                        @RequestParam ZonedDateTime to);
 }
